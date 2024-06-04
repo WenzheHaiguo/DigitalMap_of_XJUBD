bbca690fd7f3423a34057eca0d976df910159396
YoungSong <165146277+WenzheHaiguo@users.noreply.github.com>
2024-06-05 01:02:22 +0800

    重构了代码 Calculation_of_optimal_path_distance.py
    重构了最佳路径计算代码，使其符合DRY原则、SOLID原则。

--- Calculation_of_optimal_path_distance.py
+++ Calculation_of_optimal_path_distance.py
@@ -8,79 +8,68 @@ Created on Mon May 27 14:48:06 2024
 
 """
 
-import urllib.request  # 发送请求
-from urllib import parse  # URL编码
-import json  # 解析json数据
-from openpyxl import load_workbook  # 从Excel中读取镇街名称
+import urllib.request
+from urllib import parse
+import json
+from openpyxl import load_workbook
 from time import sleep
-import openpyxl
 
-nameList = []  # 创建一个列表用于接收数据
-book = load_workbook("J:\Personal file\Software programmer\新大博达.xlsx")  # 打开文件
-nameSheet = book["data"]  # 读取工作表
-# 按行读取第一列，并存入列表：
-for row in range(1, nameSheet.max_row + 1):
-    nameList.append(str(nameSheet["A%d" % row].value))
-    
-print(nameList)
+# 常量定义
+API_KEY = "dcb33fc98e98d046a94071c2bc1456c4"
+CITY = "乌鲁木齐"
+EXCEL_FILE_PATH_NAMES = "J:\\Personal file\\Software programmer\\新大博达.xlsx"
+EXCEL_SHEET_NAME = "data"
+EXCEL_FILE_PATH_DISTANCES = "J:\\Personal file\\Software programmer\\各建筑距离.xlsx"
 
-dict = {}
-for i in nameList:
-    # 拼接请求
-    url1 = 'http://restapi.amap.com/v3/place/text?keywords=' + i + '&city=乌鲁木齐&output=json&offset=1&page=1&key=dcb33fc98e98d046a94071c2bc1456c4'
-    # 将一些符号进行URL编码
-    newUrl1 = parse.quote(url1, safe="/:=&?#+!$,;'@()*[]")
-    # 发送请求
-    response1 = urllib.request.urlopen(newUrl1)
-    # 读取数据
-    data1 = response1.read()
-    # 解析json数据
-    jsonData1 = json.loads(data1)
-    # pois→0→location得到经纬度，写入字典
-    dict[i] = jsonData1['pois'][0]['location']
-    # 拆分字符串，逗号之前是经度，逗号之后是纬度
-    locations = dict[i].split(",")
+# 函数定义
+def read_excel_data(file_path, sheet_name):
+    wb = load_workbook(file_path)
+    ws = wb[sheet_name]
+    data = [ws[f"A{i+1}"].value for i in range(1, ws.max_row + 1) if ws[f"A{i+1}"].value is not None]
+    wb.close()
+    return data
 
-print(dict)
+def get_encoded_url(base_url, params):
+    return parse.quote(base_url.format(**params), safe="/:=&?#+!$,;'@()*[]")
 
-distanceList = []  # 创建一个列表用于接收数据
-k = len(nameList)  # nameList列表中元素个数
-# 遍历nameList列表
-for m in range(k):
-    subList = []  # 创建一个子列表用于接收每一条数据，主要是为了后面方便创建数组
-    for n in range(k):
-        # 从nameList中得到镇街的名称，作为键，获得dict中的经纬度
-        origin = dict[nameList[m]]
-        destination = dict[nameList[n]]
-        # 拼接请求
-        url2 = 'https://restapi.amap.com/v3/direction/driving?origin=' + origin + '&destination=' + destination + '&extensions=all&strategy=5&output=json&key=dcb33fc98e98d046a94071c2bc1456c4'
-        print(url2)
-        # 编码
-        newUrl2 = parse.quote(url2, safe="/:=&?#+!$,;'@()*[]")
-        # 发送请求
-        response2 = urllib.request.urlopen(newUrl2)
-        # 接收数据strategy
-        data2 = response2.read()
-        # 解析json文件
-        jsonData2 = json.loads(data2)
-        # 从json文件中提取距离
-        distance = jsonData2['route']['paths'][0]['distance']
-        # 将距离写入子列表
-        distanceList.append(int(distance))
-        # 看一下得到的数据，这一行有没有无所谓
-        print(nameList[m], nameList[n], distance)
-    sleep(0.2)
+def fetch_json_data(url):
+    with urllib.request.urlopen(url) as response:
+        return json.loads(response.read())
 
-print(distanceList)
+def get_location(name):
+    url = "http://restapi.amap.com/v3/place/text?keywords={}&city={}&output=json&offset=1&page=1&key={}"
+    encoded_url = get_encoded_url(url, {"keywords": name, "city": CITY, "key": API_KEY})
+    return fetch_json_data(encoded_url)["pois"][0]["location"]
 
-# 先打开我们的目标表格，再打开我们的目标表单
-wb = openpyxl.load_workbook(r"J:\Personal file\Software programmer\各建筑距离.xlsx")
-ws = wb['Sheet1']
-# 取出distance_list列表中的每一个元素，openpyxl的行列号是从1开始取得，所以我这里i从1开始取
-for i in range(1, len(distanceList) + 1):
-    distance = distanceList[i-1]
-    # 写入位置的行列号可以任意改变，这里我是从第2行开始按行依次插入第11列
-    ws.cell(row=i, column=3).value = distance
-#     print("操作成功")
-#  保存操作
-wb.save(r"J:\Personal file\Software programmer\各建筑距离.xlsx")
+def calculate_distance(origin, destination):
+    url = "https://restapi.amap.com/v3/direction/driving?origin={}&destination={}&extensions=all&strategy=5&output=json&key={}"
+    encoded_url = get_encoded_url(url, {"origin": origin, "destination": destination, "key": API_KEY})
+    return fetch_json_data(encoded_url)["route"]["paths"][0]["distance"]
+
+def write_distances_to_excel(distances, file_path):
+    wb = load_workbook(file_path)
+    ws = wb["Sheet1"]
+    for i, distance in enumerate(distances, start=1):
+        ws.cell(row=i, column=3).value = distance
+    wb.save(file_path)
+    wb.close()
+
+# 主逻辑
+def main():
+    name_list = read_excel_data(EXCEL_FILE_PATH_NAMES, EXCEL_SHEET_NAME)
+    locations = {name: get_location(name) for name in name_list}
+    distance_list = []
+
+    for i in range(len(name_list)):
+        for j in range(i+1, len(name_list)):
+            origin = locations[name_list[i]]
+            destination = locations[name_list[j]]
+            distance = calculate_distance(origin, destination)
+            distance_list.append(distance)
+            print(f"Distance between {name_list[i]} and {name_list[j]}: {distance}")
+
+    write_distances_to_excel(distance_list, EXCEL_FILE_PATH_DISTANCES)
+    print("Distances have been calculated and saved.")
+
+if __name__ == "__main__":
+    main()
